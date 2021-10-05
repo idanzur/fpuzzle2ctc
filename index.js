607e223ac657a5923b8693438b62955f44c65038
idanzur <38559851+idanzur@users.noreply.github.com>
2021-10-05 21:41:04 +0300

    change to window.location

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ redirect = () => {
     const ctcBase = 'https://app.crackingthecryptic.com/sudoku/?puzzleid=fpuzzles'
     const loadParam = document.querySelector("input").value.split('?load=')[1]
     const fpuzzleData = encodeURIComponent(loadParam)
-    window.open(`${ctcBase}${fpuzzleData}`, '_blank').focus()
-}
+    window.location = `${ctcBase}${fpuzzleData}`
